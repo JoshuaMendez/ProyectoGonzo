519329f87cae09d67eaa10799ed49a41d8f1924a
sebasizq <sebastiansaavedra_2005@hotmail.com>
2023-09-04 16:02:51 -0500

    first version

--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-int estadisticaPersonajes[3][5]={{50,35,70,20},{20,40,95,60},{40,55,87,70}};
+
+/*                                 Calonso        Guty          LaRata    */
+int estadisticasPersonajes[3][4]={{50,35,70,20},{20,40,95,60},{40,55,87,70}};
+/*                            Fuerza, Dureza Física, Dureza Mágica, Inteligencia */
+
 int elegirPersonaje(){
     int personaje;
     printf("Elije tu personaje:\n1:Calonso\n2:Guty\n3.LaRata\n");
     scanf("%d",&personaje);
+    personaje-=1;
     return personaje;
 }
-int jugar(int personaje){
+int menuPrincipal(int personaje){
     int opc;
-    
-    switch (opc)
-    {
-    case /* constant-expression */:
-        /* code */
+    printf("Elija la opción que deseas realizar: \n1.Explorar\n2.Deck\n3.Bestiario\n4.Salir\n");
+    scanf("%d",opc);
+    switch (opc){
+    case 1:
+        /* explorar */
+        break;
+    case 2:
+        /* Deck */
+        break;
+    case 3:
+        /* Bestiario */
+        break;
+    case 4:
+        /* explorar */
         break;
     
     default:
@@ -37,14 +51,14 @@ void menu(){
             
             case 2:
                 printf("Fin del juego\n");
+                exit = 1;
                 break;
             }
         }
         else{
             printf("1. Nuevo juego\n2.Continuar\n2.Salir\n");
             scanf("%d",&opc);
-            switch (opc)
-            {
+            switch (opc){
             case 1:
                 personajeSelect=elegirPersonaje();
                 jugar(personajeSelect);
@@ -55,6 +69,7 @@ void menu(){
                 break;
             case 3:
                 printf("Fin del juego\n");
+                exit = 1;
                 break;
             }
         }
